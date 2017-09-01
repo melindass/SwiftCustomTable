e466f3f95781a677ef58591dc8834c54e38a97d9
亚洲金控 <unpcn@iMac.local>
2017-09-01 17:05:59 +0800

    swift版自定义TabelView和CollectionView

---
+++ SwiftCustomTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCustomTable
+//
+//  Created by 亚洲金控 on 2017/9/1.
+//  Copyright © 2017年 melinday. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+          self.title = "自定义表格";
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftCustomTable/customCell.swift
@@ -0,0 +1,24 @@
+//
+//  customCell.swift
+//  SwiftCustomTable
+//
+//  Created by 亚洲金控 on 2017/9/1.
+//  Copyright © 2017年 melinday. All rights reserved.
+//
+
+import UIKit
+
+class customCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
